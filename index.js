e8f70fb29ceba5a6aad84e76d0b3ad7a1feb2a29
Trevor-Okwirri <71628204+Trevor-Okwirri@users.noreply.github.com>
2023-03-08 06:01:07 +0300

    Update index.js

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const cors = require("cors");
 dotenv.config();
 
 mongoose
-  .connect("mongodb+srv://trevorokwirri:trevor@1234@anonymous-justice.eppsouf.mongodb.net/?retryWrites=true&w=majority/anonymous-justice")
+  .connect("mongodb+srv://trevorokwirri:trevor%401234@anonymous-justice.eppsouf.mongodb.net/?retryWrites=true&w=majority/anonymous-justice")
   .then(() => console.log("DB Connection Successfull!"))
   .catch((err) => {
     console.log(err);
